cb40709f45923b7e34db3607674f402a1fac2635
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2026-06-03 16:18:56 +0700

    build: initialize project

---
+++ apps/server/src/index.ts
@@ -0,0 +1,54 @@
+import { devToolsMiddleware } from "@ai-sdk/devtools";
+import { google } from "@ai-sdk/google";
+import { auth } from "@grammar-correction-tool/auth";
+import { env } from "@grammar-correction-tool/env/server";
+import { streamText, convertToModelMessages, wrapLanguageModel } from "ai";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(
+  "/*",
+  cors({
+    origin: env.CORS_ORIGIN,
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
+
+app.post("/ai", async (c) => {
+  const body = await c.req.json();
+  const uiMessages = body.messages || [];
+  const model = wrapLanguageModel({
+    model: google("gemini-2.5-flash"),
+    middleware: devToolsMiddleware(),
+  });
+  const result = streamText({
+    model,
+    messages: await convertToModelMessages(uiMessages),
+  });
+
+  return result.toUIMessageStreamResponse();
+});
+
+app.get("/", (c) => {
+  return c.text("OK");
+});
+
+import { serve } from "@hono/node-server";
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,33 @@
+import { createDb } from "@grammar-correction-tool/db";
+import * as schema from "@grammar-correction-tool/db/schema/auth";
+import { env } from "@grammar-correction-tool/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [],
+  });
+}
+
+export const auth = createAuth();
